b259661ddcb045725d96f4f4fb35ca1e4b34e81d
Bjarke Bondo Andersen <bba@secoya.dk>
2019-01-09 12:08:06 +0100

    Proof-of-concept for Orbit editor

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { Editor } from 'react-draft-wysiwyg';
-import { convertToRaw, ContentState, EditorState } from 'draft-js';
+import { convertToRaw, CompositeDecorator, ContentState, EditorState } from 'draft-js';
 import draftToHtml from 'draftjs-to-html';
 import htmlToDraft from './library';
 import '../node_modules/react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
@@ -10,37 +10,123 @@ import './styles.css';
 // in constructor, I use your code above, but I change outputEditorState to inputEditorState
 // in the first Editor, I use this.state.inputEditorState as editorState
 
-class Playground extends Component {
+const orbitBlockDecoder = (nodeName, node) => {
+  if (nodeName === 'img' && node instanceof HTMLImageElement) {
+    const entityConfig = {};
+    entityConfig.src = node.getAttribute ? node.getAttribute('src') || node.src : node.src;
+    entityConfig.alt = node.alt;
+    entityConfig.height = node.style.height;
+    entityConfig.width = node.style.width;
+    if (node.style.float) {
+      entityConfig.alignment = node.style.float;
+    }
+    if (node.dataset.news_imageFileId) {
+      entityConfig.orbitNewsFileId = parseInt(node.dataset.news_imageFileId, 10);
+      entityConfig.orbitClassName = node.className;
+    }
+    return {
+      type: 'IMAGE',
+      mutability: 'MUTABLE',
+      data: entityConfig,
+    };
+  }
+};
+const orbitInlineDecoder = (nodeName, node) => {
+  if (nodeName === 'a' && node instanceof HTMLAnchorElement && node.dataset.linktype && node.dataset.linkid) {
+    const entityConfig = {};
+    entityConfig.url = node.getAttribute ? node.getAttribute('href') || node.href : node.href;
+    entityConfig.title = node.innerHTML;
+    entityConfig.targetOption = node.target;
+    entityConfig.orbitLinkType = node.dataset.linktype;
+    entityConfig.orbitLinkId = parseInt(node.dataset.linkid, 10);
+    return {
+      type: 'LINK',
+      mutability: 'MUTABLE',
+      data: entityConfig,
+    };
+  }
+};
+const orbitLinkDecoratorObject = {
+  strategy: (contentBlock, callback, contentState) => {
+    contentBlock.findEntityRanges(character => {
+      const entityKey = character.getEntity();
+      if (entityKey == null) {
+        return false;
+      }
+      const entity = contentState.getEntity(entityKey);
+      const data = entity.getData();
+      return data.orbitLinkType != null && data.orbitLinkId != null && entity.getType() === 'LINK';
+    }, callback);
+  },
+  component: props => {
+    const entity = props.contentState.getEntity(props.entityKey);
+    const data = entity.getData();
+    return (
+      <a title={data.title} href={data.url} data-linktype={data.orbitLinkType} data-linkid={data.orbitLinkId}>
+        ("{props.decoratedText}")
+      </a>
+    );
+  },
+};
+const orbitLinkDecorator = new CompositeDecorator([orbitLinkDecoratorObject]);
 
+const convertToHTML = editorContent =>
+  draftToHtml(editorContent, null, null, (entity, text) => {
+    if (entity.type === 'LINK' && entity.data.orbitLinkType != null && entity.data.orbitLinkId != null) {
+      const targetOption = entity.data.targetOption || '_self';
+      const orbitLinkType = entity.data.orbitLinkType;
+      const orbitLinkId = entity.data.orbitLinkId;
+      return `<a href="${
+        entity.data.url
+      }" target="${targetOption}" data-linktype="${orbitLinkType}" data-linkid="${orbitLinkId}">${text}</a>`;
+    }
+    if (entity.type === 'IMAGE' && entity.data.orbitNewsFileId != null) {
+      const { orbitNewsFileId, orbitClassName, src, alt, alignment, height, width } = entity.data;
+
+      const style = [
+        alignment ? `float:${alignment}` : null,
+        height ? `height:${height}` : null,
+        width ? `width:${width}` : null,
+      ]
+        .filter(s => s != null)
+        .join(';');
+
+      return `<img src="${src}" alt="${alt}" style="${style}" class="${orbitClassName}" data-news_image-file-id="${orbitNewsFileId}" />`;
+    }
+    return null;
+  });
+
+class Playground extends Component {
   // state = {
   //   outputEditorState: undefined,
   // }
 
   constructor(props) {
-    super(props)
-    const html = '';
-    const contentBlock = htmlToDraft(html);
+    super(props);
+    const html = `Hello <a href="/wiki/articles/45" data-linktype="wiki" data-linkid="45">link</a> and image<div><img src="https://orbit.secoya.dk/rest/fileexplorer/filesystem/files/710/download" class="full-width" style="height: 649px;" data-news_image-file-id="710"><br></div>`;
+    // const html = `Hi <a href="/wiki/articles/45" data-linktype="wiki" data-linkid="45">link</a> and <a href="www.google.com">other</a>.`;
+    const contentBlock = htmlToDraft(html, orbitBlockDecoder, orbitInlineDecoder);
     if (contentBlock) {
       const contentState = ContentState.createFromBlockArray(contentBlock.contentBlocks, contentBlock.entityMap);
-      const inputEditorState = EditorState.createWithContent(contentState);
+      const inputEditorState = EditorState.createWithContent(contentState, orbitLinkDecorator);
       this.state = {
         inputEditorState,
       };
     }
   }
 
-  onInputEditorChange = (inputEditorState) => {
-    console.log('into onInputEditorChange')
+  onInputEditorChange = inputEditorState => {
+    console.log('into onInputEditorChange');
     // console.log('*****', inputEditorState.getCurrentContent())
     const rawContent = convertToRaw(inputEditorState.getCurrentContent());
-    const html = draftToHtml(rawContent);
-    console.log('html', html)
-    const contentBlock = htmlToDraft(html);
-//    console.log('1', contentBlock)
+    const html = convertToHTML(rawContent);
+    console.log('html', html);
+    const contentBlock = htmlToDraft(html, orbitBlockDecoder, orbitInlineDecoder);
+    //    console.log('1', contentBlock)
     // console.log('2', convertFromHTML(html) && convertFromHTML(html))
     if (contentBlock) {
       const contentState = ContentState.createFromBlockArray(contentBlock.contentBlocks);
-      const outputEditorState = EditorState.createWithContent(contentState);
+      const outputEditorState = EditorState.createWithContent(contentState, orbitLinkDecorator);
       this.setState({
         inputEditorState,
         outputEditorState,
@@ -48,50 +134,36 @@ class Playground extends Component {
       // console.log('1', inputEditorState.getCurrentContent().getBlocksAsArray())
       // console.log('2', contentBlock.contentBlocks)
     }
-  }
+  };
 
   render() {
-    // console.log('*****', this.state.inputEditorState.getCurrentContent())
-    // value={this.state.inputEditorState && draftToHtml(convertToRaw(this.state.inputEditorState.getCurrentContent()))}
+    // console.log('*****', this.state.inputEditorState.getCurrentContent().toJS());
+    // value={this.state.inputEditorState && convertToHTML(convertToRaw(this.state.inputEditorState.getCurrentContent()))}
     return (
       <div>
         <div style={{ height: 200 }}>
           <Editor
+            customDecorators={[orbitLinkDecoratorObject]}
             editorState={this.state.inputEditorState}
             onEditorStateChange={this.onInputEditorChange}
-            mention={{
-              separator: ' ',
-              trigger: '@',
-              suggestions: [
-                { text: 'A', value: 'a', url: 'href-a' },
-                { text: 'AB', value: 'ab', url: 'href-ab' },
-                { text: 'ABC', value: 'abc', url: 'href-abc' },
-                { text: 'ABCD', value: 'abcd', url: 'href-abcd' },
-                { text: 'ABCDE', value: 'abcde', url: 'href-abcde' },
-                { text: 'ABCDEF', value: 'abcdef', url: 'href-abcdef' },
-                { text: 'ABCDEFG', value: 'abcdefg', url: 'href-abcdefg' },
-              ],
-            }}
           />
         </div>
         <div style={{ height: 200 }}>
           <textarea
             disabled
             className="demo-content"
-            value={this.state.inputEditorState && draftToHtml(convertToRaw(this.state.inputEditorState.getCurrentContent()))}
+            value={
+              this.state.inputEditorState &&
+              convertToHTML(convertToRaw(this.state.inputEditorState.getCurrentContent()))
+            }
           />
         </div>
         <div style={{ height: 200 }}>
-        <Editor
-          editorState={this.state.outputEditorState}
-        />
+          <Editor customDecorators={[orbitLinkDecoratorObject]} editorState={this.state.outputEditorState} />
         </div>
       </div>
     );
   }
 }
 
-ReactDOM.render(
-  <Playground />,
-  document.getElementById('root')
-);
+ReactDOM.render(<Playground />, document.getElementById('root'));
--- src/library/getEntityId.js
+++ src/library/getEntityId.js
@@ -1,32 +1,31 @@
+/* @flow */
+
 import { Entity } from 'draft-js';
 
-const getEntityId = (node) => {
+const getEntityId = (node: HTMLElement, customInlineChunkGenerator: ?CustomChunkGenerator) => {
   let entityId = undefined;
-  if (
-    node instanceof HTMLAnchorElement
-  ) {
+  if (customInlineChunkGenerator) {
+    const nodeName = node.nodeName.toLowerCase();
+    const value = customInlineChunkGenerator(nodeName, node);
+    if (value) {
+      return Entity.__create(value.type, value.mutability, value.data || {});
+    }
+  }
+  if (node instanceof HTMLAnchorElement) {
     const entityConfig = {};
     if (node.dataset && node.dataset.mention !== undefined) {
       entityConfig.url = node.href;
       entityConfig.text = node.innerHTML;
       entityConfig.value = node.dataset.value;
-      entityId = Entity.__create(
-        'MENTION',
-        'IMMUTABLE',
-        entityConfig,
-      );
+      entityId = Entity.__create('MENTION', 'IMMUTABLE', entityConfig);
     } else {
       entityConfig.url = node.getAttribute ? node.getAttribute('href') || node.href : node.href;
       entityConfig.title = node.innerHTML;
       entityConfig.targetOption = node.target;
-      entityId = Entity.__create(
-        'LINK',
-        'MUTABLE',
-        entityConfig,
-      );
+      entityId = Entity.__create('LINK', 'MUTABLE', entityConfig);
     }
   }
   return entityId;
-}
+};
 
 export default getEntityId;
